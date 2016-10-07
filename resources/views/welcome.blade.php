28a80b2faeff5c515e4976fddb20ff9be2b5b81a
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-07 12:01:55 +0100

    added bootstrap template
    Admin LTE template imported and some minor adjustments made to welcome
    view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,181 @@
+
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-        <title>Laravel</title>
+    <title>myaccount School Portal</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- social network metas -->
+    <meta property="image" content="http://almsaeedstudio.com/adminlte2.png"/>
+    <meta property="og:image" content="http://almsaeedstudio.com/adminlte2.png"/>
+    <meta property="site_name" content="myaccount School Portal"/>
+    <meta property="description" content="Prototype of myaccount School Portal for accessing online school services." />
+    <meta name="description" content="Prototype of myaccount School Portal for accessing online school services." />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <link rel="icon" type="image/png" href="http://almsaeedstudio.com/logo.png">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <style>
+        html, body {
+            min-height: 100%;
+            padding: 0;
+            margin: 0;
+            font-family: 'Source Sans Pro', "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
+        iframe {
+            display: block;
+            overflow: auto;
+            border: 0;
+            margin: 0;
+            padding: 0;
+            margin: 0 auto;
+        }
+        .frame {
+            height: 49px;
+            margin: 0;
+            padding: 0;
+            border-bottom: 1px solid #ddd;
+        }
+        .frame a {
+            color: #666;
+        }
+        .frame a:hover {
+            color: #222;
+        }
+        .frame .buttons a {
+            height: 49px;
+            line-height: 49px;
+            display: inline-block;
+            text-align: center;
+            width: 50px;
+            border-left: 1px solid #ddd;
+        }
+        .frame .brand {
+            color: #444;
+            font-size: 20px;
+            line-height: 49px;
+            display: inline-block;
+            padding-left: 10px;
+        }
+        .frame .brand small {
+            font-size: 14px;
+        }
+        a,a:hover {
+            text-decoration: none;
+        }
+        .container-fluid {
+            padding: 0;
+            margin: 0;
+        }
+        .text-muted {
+            color: #999;
+        }
+        .ad {
+            text-align: center;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            background: #222;
+            background: rgba(0,0,0,0.8);
+            width: 100%;
+            color: #fff;
+            display: none;
+        }
+        #close-ad {
+            float: left;
+            margin-left: 10px;
+            margin-top: 10px;
+            cursor: pointer;
+        }
+    </style>
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-            .position-ref {
-                position: relative;
-            }
+        ga('create', 'UA-46680343-1', 'almsaeedstudio.com');
+        ga('send', 'pageview');
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    </script>
+</head>
+<body>
+<header>
+    <nav class="frame" role="navigation">
+        <div class="container-fluid">
+            <a href="" class="brand">
+                myaccount
+                <small class="text-muted hidden-xs">School Portal Prototype</small>
+            </a>
+            <div class="buttons pull-right">
+                <a class="first hidden-xs" id="display-full" href="#" data-toggle="tooltip" data-placement="bottom" title="Display Desktop - full width"><i class="fa fa-desktop fa-lg"></i></a>
+                <a class="hidden-xs" id="display-940" href="#" data-toggle="tooltip" data-placement="bottom" title="Display Tablet - 940px"><i class="fa fa-tablet fa-lg"></i></a>
+                <a class="hidden-xs" id="display-480" href="#" data-toggle="tooltip" data-placement="bottom" title="Display Phone - 480px"><i class="fa fa-mobile fa-lg"></i></a>
+                <a href="https://www.almsaeedstudio.com/premium" data-toggle="tooltip" data-placement="bottom" title="Premium Templates"><i  style="color: #F34D59" class="fa fa-tint fa-lg"></i></a>
+                <a href="https://www.almsaeedstudio.com/download/AdminLTE-master" data-toggle="tooltip" data-placement="bottom" title="Download now"><i class="fa fa-download fa-lg"></i></a>
+                <a id="remove-frame" href="#" data-toggle="tooltip" data-placement="bottom" title="Remove frame"><i class="fa fa-times"></i></a>
             </div>
-        </div>
-    </body>
+        </div><!-- /.container -->
+    </nav><!--/.navbar-->
+</header>
+<iframe src="//almsaeedstudio.com/themes/AdminLTE/index2.html" id="preview-iframe"></iframe>
+
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script>
+    $(function() {
+        function _fix() {
+            var h = $(window).height();
+            var w = $(window).width();
+            $("#preview-iframe").css({
+                width: w + "px",
+                height: (h - 50) + "px"
+            });
+        }
+        _fix();
+        $(window).resize(function() {
+            _fix();
+        });
+        $('[data-toggle="tooltip"]').tooltip();
+
+        function iframe_width(width) {
+            $("#preview-iframe").animate({width: width}, 500);
+        }
+
+        $("#display-full").click(function(e){
+            e.preventDefault();
+            iframe_width("100%");
+        });
+
+        $("#display-940").click(function(e){
+            e.preventDefault();
+            iframe_width("940px");
+        });
+
+        $("#display-480").click(function(e){
+            e.preventDefault();
+            iframe_width("480px");
+        });
+
+        $("#remove-frame").click(function(e){
+            e.preventDefault();
+            window.location.href = "http://almsaeedstudio.com/themes/AdminLTE/index2.html";
+        });
+
+    });
+
+</script>
+</body>
 </html>
