879bc02e24607b24f7394643a5b9bf83150427d0
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-09 12:02:52 +0100

    Template and models updated
    Templated added to welcome blade and models generated

---
+++ resources/layouts/head.blade.php
@@ -0,0 +1,39 @@
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{!! csrf_token() !!}">
+
+        <link href="./css/app.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="./css/custom.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="http://bootswatch.com/assets/css/custom.min.css" rel="stylesheet" type="text/css">
+        <link href="http://bootswatch.com/flatly/bootstrap.min.css" rel="stylesheet"  type="text/css">
+
+        <!-- jquery -->
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script type="text/javascript" async="" src="http://www.google-analytics.com/ga.js"></script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+       <!--  // <script type="text/javascript" src="./js/ajax-crud.js"></script>
+        // <script type="text/javascript" src="./js/feedback-crud.js"></script>
+        // <script type="text/javascript" src="./js/absence-crud.js"></script>
+        // <script type="text/javascript" src="./js/enrolment-crud.js"></script> -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.0.1/vue-resource.min.js"></script>
+        <script type="text/javascript" src="/js/app.js"></script>
+        
+
+
+
+
+
+        <title> @yield('title')</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- handlebars -->
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.0/handlebars.js"></script>
+ 
+        <!-- alpaca -->
+        <link type="text/css" href="//code.cloudcms.com/alpaca/1.5.18/bootstrap/alpaca.min.css" rel="stylesheet" />
+        <script type="text/javascript" src="//code.cloudcms.com/alpaca/1.5.18/bootstrap/alpaca.min.js"></script>
+
