fbf5cd078db78449e4fe4227cb581ebdec70ba43
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-10 10:55:19 +0100

    Models updated
    Each model now references the correct database table

--- app/Models/LocalAuthority.php
+++ app/Models/LocalAuthority.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class LocalAuthority extends Model
 {
-    //
+    protected $table = 'portal_local_authorities_mst';
+
+    protected $primaryKey = 'local_authority_id';
+
+	public $timestamps = true;
+
+    protected $fillable = [
+        'la_name',
+        'cag_code',
+        'address_lines',
+        'post_town',
+        'post_code',
+        'uprn',
+        'x_coord',
+        'y_coord',
+        'lat_val',
+        'long_val',
+        'telephone',
+        'email_address',
+        'website'
+    ];
+
+    protected $guarded = [];
 }
