a5794a97cbd6730ea7f2a3d2da42e0135ea890e6
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-11 10:32:30 +0100

    sidebar toggle working
    Updated filepath and now js is working on sidebar toggle menu

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -25,7 +25,7 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script type="text/javascript" src="./dist/app.js"></script>
+<script type="text/javascript" src="./dist/js/app.js"></script>
 <script type="text/javascript" async="" src="http://www.google-analytics.com/ga.js"></script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="./js/ajax-crud.js"></script>
