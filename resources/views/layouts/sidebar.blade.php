94e7c0ff8a7025711d3d8281b5fa6cf96ab9fc1c
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-19 10:10:22 +0100

    Further Sidebar Changes
    Altered some of the menu structure and added links to user guide pages

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -73,12 +73,8 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
-            <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
-            <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
-            <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
-            <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
-            <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
+            <li><a href="/payment">Make a payment</a></li>
+            <li><a href="/mealcalendar">View Attendance Record</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -109,21 +105,14 @@
         </li>
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-question-circle"></i> <span>User Guide</span>
+            <i class="fa fa-mouse-pointer"></i> <span>User Guides</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/examples/invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
-            <li><a href="pages/examples/profile.html"><i class="fa fa-circle-o"></i> Profile</a></li>
-            <li><a href="pages/examples/login.html"><i class="fa fa-circle-o"></i> Login</a></li>
-            <li><a href="pages/examples/register.html"><i class="fa fa-circle-o"></i> Register</a></li>
-            <li><a href="pages/examples/lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
-            <li><a href="pages/examples/404.html"><i class="fa fa-circle-o"></i> 404 Error</a></li>
-            <li><a href="pages/examples/500.html"><i class="fa fa-circle-o"></i> 500 Error</a></li>
-            <li><a href="pages/examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
-            <li><a href="pages/examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
+            <li><a href="/help"><i class="fa fa-question-circle"></i>Help</a></li>
+            <li><a href="/accessibility"><i class="fa fa-wheelchair"></i> Accessibility</a></li>
           </ul>
         </li>
         <!-- <li class="treeview">
