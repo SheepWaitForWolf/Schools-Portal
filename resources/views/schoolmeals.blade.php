f3d7f6b30b3a0f48423622ac8c8317cabe4592f1
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-20 14:41:21 +0100

    School Meals content added
    Updated schoolmeals blade with content

--- resources/views/schoolmeals.blade.php
+++ resources/views/schoolmeals.blade.php
@@ -4,3 +4,23 @@
 School Meals
 @stop
 
+@section('Content')
+<br>
+<br>
+<div class="container">
+	<div class="col-md-8">
+		<p> <strong>Free School Meals</strong><br>
+			<br>
+		<br>
+		<p> 
+Since 5th January 2015 all P1-P3 pupils are entitled to a Free School Meal.
+In addition Free school meals will be granted to all other pupils whose parents/carers are in receipt of either Income
+Support* or Income Based Job Seekers Allowance* or **Child Tax Credit* or **Working Tax Credit and Child Tax
+Credit* or Employment and Support Allowance (income related)* or Universal Credit or Support under PartVI of the
+Immigration and Asylum Act 1999(see**notes for entitlement)
+* Pupils are eligible to apply for free school meals in their own right if they are aged 16 to 18, and receive one of these
+benefits.</p> 
+
+	</div>
+</div>
+@stop
