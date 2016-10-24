48d9d03509fac096ee4f69092f3f5db8f6f0c81e
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-24 09:10:25 +0100

    head file edits
    amended included files in head to allow javascript calender and sidebar
    toggle to work simultaneously

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -38,7 +38,7 @@
 
 
 <!-- jQuery UI 1.11.4 -->
-<!-- <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script> -->
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <!-- Slimscroll -->
 <script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
