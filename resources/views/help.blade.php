948d5627a0a30969fda6762665bc8951cfe24c00
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-25 12:02:25 +0100

    schoolmeals update
    I rearranged the content on the schoolmeals page for readability and
    added the free school meals text description to the help page

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -21,6 +21,15 @@ Help
 	 	solutions we see how they change over time allowing us to adapt and adjust schoolportal.com to ensure it’s simple and
 	 	 easy to use. </p> 
 
+	 	 <h3>Free School Meals</h3>
+
+	 	 <p>Since 5th January 2015 all P1-P3 pupils are entitled to a Free School Meal. 
+	 	 	In addition Free school meals will be granted to all other pupils whose parents/carers 
+	 	 	are in receipt of either Income Support* or Income Based Job Seekers Allowance* or **Child Tax Credit* or 
+	 	 	**Working Tax Credit and Child Tax Credit* or Employment and Support Allowance (income related)* or Universal Credit or 
+	 	 	Support under PartVI of the Immigration and Asylum Act 1999(see**notes for entitlement) * Pupils are eligible
+	 	 	to apply for free school meals in their own right if they are aged 16 to 18, and receive one of these benefits.
+	 	 </p>
 	</div>
 </div>
 @stop
