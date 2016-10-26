18e770e4876505df3d1d4d59793b834e44efe0f0
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-26 15:53:42 +0100

    Home Controller update
    displayUserInfo function added to home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Form;
+
 use Illuminate\Http\Request;
+use App\Models\Child;
+use App\Models\Feedback;
+use App\Models\LocalAuthority;
+use App\Models\Absence;
+use App\Models\School;
+use App\Http\Requests;
+use Response;
+use DB;
+use Log;
 use Auth;
 
 class HomeController extends Controller
@@ -12,10 +27,6 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
@@ -38,16 +49,26 @@ class HomeController extends Controller
         {
         return redirect('/home');
         }
-
+        else {
         return redirect('/login')->withErrors([
             'email' => 'The credentials you entered did not match our records. Try again?',
             ]);
+    }
     }
 
     public function logout()
     {
         Auth::logout();
         return view('auth.login');
+    }
+
+    public function displayUserInfo(Request $request)
+    {
+        $inboxcount = 4;
+        $childcount = count(DB::table('portal_child_mst')->select('*')->get());
+         //DB::select('SELECT SUBSTRING_INDEX(name, " ", 1) FROM portal_users_mst WHERE email = "big_tony@gmail.com"');
+        $username = DB::table('portal_users_mst')->select('name')->where('email', '=', 'big_tony@gmail.com')->get();
+        return view('home')->with('childcount', $childcount)->with('username', $username);
     }
 
 }
