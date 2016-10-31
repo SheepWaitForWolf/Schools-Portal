ff1102b0bea56109b47362129b1c6d15089a9728
SheepWaitForWolf <dglennie89@gmail.com>
2016-10-31 09:44:33 +0000

    sidebar icon update
    altered some icons on the sidebar for consistency and uniqueness

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -53,7 +53,7 @@
         </li>
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-calendar-check-o"></i>
+            <i class="fa fa-calendar"></i>
             <span>Attendance</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
